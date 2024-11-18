76aca7216ea3af175406764735c0958aa2db94ba
DmitryTaushev <123morfix@gmail.com>
2024-11-18 09:27:44 +0500

    При помощи функции os.walk() вывели в консоль содержимое папки моего нового проекта

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
 
-path_learning = os.path.normpath(r'C:/Users/123mo/Desktop/HW_module_6_file_TaushevDN/data_path_1/test_file_1.txt')
+path_learning = os.path.normpath(r'C:/Users/123mo/Desktop/HW_module_6_file_TaushevDN')
 print(path_learning)
+
+for path,dirnames,filenames in os.walk(path_learning):
+    print(f"path - {path}")
+    print(f"dirnames - {dirnames}")
+    print(f"filenames - {filenames}")
