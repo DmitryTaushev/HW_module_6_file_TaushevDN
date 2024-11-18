e050dd7a753c365c05fa76c72be8bb7704099343
DmitryTaushev <123morfix@gmail.com>
2024-11-18 09:59:10 +0500

    Написали функцию которая записала текст согласно ТЗ

---
+++ task_2_file_format.py
@@ -0,0 +1,14 @@
+
+
+list_text = ['Если б мишки были пчелами,','То они бы нипочем,','Никогда и не подумали,','Так высоко строить дом.']
+def write_text(filename):
+    with open(f'{filename}.txt','w',encoding = 'utf-8') as text:
+        count = 0
+        for i in list_text:
+            text.write(f'{list_text[count]}\n')
+            count += 1
+
+filename = 'HW_text'
+
+write_text(filename)
+
