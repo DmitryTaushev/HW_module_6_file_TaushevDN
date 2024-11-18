678085ab2f8a41bd5c0b972ccd134f34acb501e7
DmitryTaushev <123morfix@gmail.com>
2024-11-18 09:33:07 +0500

    При помощи метода os.path.join() cоставил нормализованный абсолютный путь к файлу test_file_3.txt

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ for path,dirnames,filenames in os.walk(path_learning):
     print(f"path - {path}")
     print(f"dirnames - {dirnames}")
     print(f"filenames - {filenames}")
+
+disk = 'C:\\'
+dir_1 = 'Users'
+dir_2 = '123mo'
+dir_3 = 'Desktop'
+dir_4 = 'HW_module_6_file_TaushevDN'
+dir_5 = 'data_path_2'
+dir_6 = 'test_file_3.txt'
+
+path_module = os.path.join(disk,dir_1,dir_2,dir_3,dir_4,dir_5,dir_6)
+print(path_module)
